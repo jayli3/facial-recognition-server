5ea73c3d3f271df741bd933393b0abdb0016d54c
Jay Li <jay.li23697@gmail.com>
2019-04-17 20:46:00 +0800

    installed psql db

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # facial-recognition-server
 Node.js server to handle our facial recognition web app.
 
-## Packages Used
+## NPM Packages Used
 Express.js<br>
-Body-Parser
+Body-Parser<br>
+Knex.js<br>
+PG
 
 ## Database
-PostgreSQL
+PostgreSQL<br>
+install postgresql with npm<br>
+terminal: createdb 'name'<br>
+psql 'name'
--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex')
+
+knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'apple',
+    password : '',
+    database : 'facial-recognition'
+  }
+});
 
 const app = express();
 const PORT = 3001;
