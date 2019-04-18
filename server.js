209f653d1d495f5caab7e2f1128b5b75a818fe8f
Jay Li <jay.li23697@gmail.com>
2019-04-18 22:38:06 +0800

    dynamic port

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const db = knex({
 });
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 	console.log('Server is running on port: ', PORT);
 });
