57c69cc96c5d6270b1d0c2aaf4857cfd27bfc0b3
Jay Li <jay.li23697@gmail.com>
2019-04-18 23:45:41 +0800

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex')
+const knex = require('knex');
 
 const registerC = require('./Controllers/Register');
 const signinC = require('./Controllers/SignIn');
